ad3468e3a17579657c88b44d8e45c1852375657d
udhin98 <udhin1998@gmail.com>
2024-01-17 12:32:48 +0100

    tipo dispositivo

--- src/Magazzino.java
+++ src/Magazzino.java
@@ -24,7 +24,7 @@ public class Magazzino {
         prodotti.add(prodotto);
     }
 
-   //TODO tipo
+
     public void inserimentoProdotto(int tipo) throws IOException {
 
         Prodotto prodotto;
